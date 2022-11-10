66ba121d7b28830e7630237cf28d761253e05384
Ayuukina <60597271+Ayuukina@users.noreply.github.com>
2022-11-10 15:35:57 +0100

    Update nbaWebScrapping.py

--- nbaWebScrapping.py
+++ nbaWebScrapping.py
@@ -11,15 +11,21 @@ def get_nba_stats():
     table = driver.find_element("xpath", '//*[@id="__next"]/div[2]/div[2]/div[3]/section[2]/div/div[2]/div[3]/table')
     # Get the rows
     rows = table.find_elements("tag name", "tr")
+    # Get the columns
+    columns = table.find_elements("tag name", "td")
     # Get the data
-    data = []
-    for row in rows:
-        data.append(row.text)
+    data = [[]]
+    for i in range(len(columns)):
+        if i % 30 == 0:
+            data.append([])
+        data[-1].append(columns[i].text)
+    data.pop(0)
+    
 
     # Close the driver
-    #driver.close()
-
+    driver.close()
     return data
 
 x=get_nba_stats()
-print(x[1])
+print(x[0])
+
